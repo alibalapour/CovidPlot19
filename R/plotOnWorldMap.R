d7409444024a3b6cd4551a79ff0b462b9a416b29
alibalapour <alibalapour93.ab@gmail.com>
2020-10-23 16:45:00 +0330

    plotOnWorldMap.R completed :)

--- R/plotOnWorldMap.R
+++ R/plotOnWorldMap.R
@@ -11,24 +11,25 @@ plotOnWorldMap <- function(date, type){
 
   df <- main_df
   df %>%
-    filter(Date == '2020-10-15') -> df
+    filter(Date == as_date(date)) -> df
 
   world <- ne_countries(scale = "medium", returnclass = "sf")
   world %>%
     mutate(Country = name) %>%
     group_by(Country) %>%
     mutate(Country = case_when(
-      Country == 'United States of America' ~ 'US',
+      Country == 'United States' ~ 'US',
       TRUE ~ Country
     )) -> world
 
+  new_df <- left_join(world, df, by = 'Country')
 
-
-  new_df <- merge(world, df, by = 'Country')
-
+  new_df %>%
+    replace_na(list(DailyConfirmed = 0, DailyDeaths = 0,
+                    Confirmed = 0, Deaths = 0)) -> new_df
 
   ggplot(data = new_df) +
-    geom_sf(aes(fill = DailyConfirmed)) +
+    geom_sf(aes_string(fill = type)) +
     scale_fill_viridis_c(option = "plasma", trans = "sqrt")
 
 }
